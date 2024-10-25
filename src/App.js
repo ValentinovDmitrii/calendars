2dfc04149e2b1942e93e7f035ba6e457e16d904b
Dmitrii Valentinov <d.valentinov@gmail.com>
2024-10-25 21:40:42 +0200

    First start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import CalendarGoogle from './CalendarGoogle'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calendars</h1>
+      <CalendarGoogle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CalendarGoogle.js
@@ -0,0 +1,10 @@
+const CalendarGoogle = () => {
+    return(
+        <div>
+        {/* <iframe class="calendarGoogle" src="https://calendar.google.com/calendar/embed?src=a8ed0fa7c8a436d52748d39dd1d38a3b9678fba73a186242b1964f53b52beb72%40group.calendar.google.com&ctz=Europe%2FVienna"></iframe> */}
+        <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3QtIHVNyGLz1rCjzqFzhuAattgvlQIyTXisM-c-166PDWLQbp-1iKWn7N-Bj4ckSwUklW0pww0?gv=true" style={{"border": "0", "width": "100%", "height": "1600px", "frameborder": "0"}}></iframe>
+        </div>
+    );
+};
+
+export default CalendarGoogle;
